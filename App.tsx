f05364ddc58525d1f745f044aff6958267691ec0
David <4661784+retyui@users.noreply.github.com>
2023-01-15 17:08:08 +0100

    init-rn-70

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+function App() {
+  return (
+    <SafeAreaView>
+      <Text>Code Push Demo</Text>
+    </SafeAreaView>
+  );
+}
+
+export default App;
